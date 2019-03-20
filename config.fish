4b686a91c942052e12dd9da8ffca5b4e41267bad
tabakazu <tkazz.bd1@gmail.com>
2019-03-21 02:22:19 +0900

    add fish config

---
+++ config.fish
@@ -0,0 +1,21 @@
+## Alias
+alias rm='rmtrash'
+
+## Ruby (rbenv)
+status --is-interactive; and source (rbenv init -|psub)
+## Python (pyenv)
+status --is-interactive; and source (pyenv init -|psub)
+## Node.js (nodenv)
+status --is-interactive; and source (nodenv init -|psub)
+## Golang (goenv)
+status --is-interactive; and source (goenv init -|psub)
+## direnv
+eval (direnv hook fish)
+
+## Original Prompt
+function fish_prompt
+  set_color purple; echo -n (basename (pwd))
+  set_color brblue; echo -n (git branch  2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ \1/')
+  set_color brcyan; echo -n " \$ "
+  set_color normal;
+end
