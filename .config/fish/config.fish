68cea1422e4aa0e659b5ca7e3820571521bb100a
tabakazu <tkazz.bd1@gmail.com>
2021-01-05 08:20:09 +0900

    remove line for path setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,6 @@
 alias rm='rmtrash'
 alias gc='git branch | peco | xargs git checkout'
 
-## Ruby (rbenv)
-status --is-interactive; and source (rbenv init -|psub)
-## Python (pyenv)
-status --is-interactive; and source (pyenv init -|psub)
-## Node.js (nodenv)
-status --is-interactive; and source (nodenv init -|psub)
-## Golang (goenv)
-status --is-interactive; and source (goenv init -|psub)
-## direnv
-eval (direnv hook fish)
-
 ## Original Prompt
 function fish_prompt
   set_color purple; echo -n (basename (pwd))
